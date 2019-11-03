50d88ac3fecef0a6a68a562f10a25e1da0993f09
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-11-03 02:23:27 -0800

    updated back end names

--- jamstack-config.js
+++ jamstack-config.js
@@ -1,17 +1,17 @@
                                                                                                      
 
 const awsmobile = {
-    "aws_project_region": "us-east-2",
-    "aws_cognito_identity_pool_id": "us-east-2:f82c896f-d288-47bb-94cb-b8b31ed04b4a",
-    "aws_cognito_region": "us-east-2",
-    "aws_user_pools_id": "us-east-2_5oUJm1Zwu",
-    "aws_user_pools_web_client_id": "rk5fks6anb8dk9j5jh53avcop",
+    "aws_project_region": "us-east-1",
+    "aws_cognito_identity_pool_id": "us-east-1:01ca4792-e711-476b-9b33-cbea710741fe",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_LotmwH37a",
+    "aws_user_pools_web_client_id": "3vavdrc46tvu21j5o2s0vnfgna",
     "oauth": {},
-    "aws_appsync_graphqlEndpoint": "https://thfnupjc6vg4vixf7k65k5a4mi.appsync-api.us-east-2.amazonaws.com/graphql",
-    "aws_appsync_region": "us-east-2",
+    "aws_appsync_graphqlEndpoint": "https://7wglxavllnf4nodwzwquuibgte.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_user_files_s3_bucket": "blog-amplifiedd63c9246ad3d49b5bfd6b688d634821f-local",
-    "aws_user_files_s3_bucket_region": "us-east-2"
+    "aws_user_files_s3_bucket": "jamstack-cmsd63c9246ad3d49b5bfd6b688d634821f-dev",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
