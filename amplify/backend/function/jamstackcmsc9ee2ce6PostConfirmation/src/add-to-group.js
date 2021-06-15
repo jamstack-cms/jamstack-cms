1ae460276f65fcc704fbc5143365e1ddb8632d9e
Lewis Donovan <lewis.donovan@sonymusic.com>
2021-06-15 12:43:30 -0500

    Readability improvements

--- amplify/backend/function/jamstackcmsc9ee2ce6PostConfirmation/src/add-to-group.js
+++ amplify/backend/function/jamstackcmsc9ee2ce6PostConfirmation/src/add-to-group.js
@@ -3,38 +3,35 @@
 exports.handler = async (event, context, callback) => {
   const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
 
-  let isAdmin = false
   const adminEmails = ["dabit3@gmail.com"]
+  const isAdmin = adminEmails.indexOf(event.request.userAttributes.email) !== -1
 
-  if (adminEmails.indexOf(event.request.userAttributes.email) !== -1) {
-    isAdmin = true
+  if (!isAdmin) {
+    callback(null, event);
+    return;
   }
 
-  if (isAdmin) {
-    const groupParams = {
-      GroupName: process.env.GROUP,
-      UserPoolId: event.userPoolId,
-    };
+  const groupParams = {
+    GroupName: process.env.GROUP,
+    UserPoolId: event.userPoolId,
+  };
 
-    const addUserParams = {
-      GroupName: process.env.GROUP,
-      UserPoolId: event.userPoolId,
-      Username: event.userName,
-    };
+  const addUserParams = {
+    GroupName: process.env.GROUP,
+    UserPoolId: event.userPoolId,
+    Username: event.userName,
+  };
 
-    try {
-      await cognitoidentityserviceprovider.getGroup(groupParams).promise();
-    } catch (e) {
-      await cognitoidentityserviceprovider.createGroup(groupParams).promise();
-    }
+  try {
+    await cognitoidentityserviceprovider.getGroup(groupParams).promise();
+  } catch (e) {
+    await cognitoidentityserviceprovider.createGroup(groupParams).promise();
+  }
 
-    try {
-      await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise();
-      callback(null, event);
-    } catch (e) {
-      callback(e);
-    }
-  } else {
+  try {
+    await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise();
     callback(null, event);
+  } catch (e) {
+    callback(e);
   }
 };
